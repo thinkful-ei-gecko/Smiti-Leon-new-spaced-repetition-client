69ae3118b7d22694fcd20d52c67523009e1cadae
Leon Dail <leondailofficial@gmail.com>
2019-11-21 10:53:17 -0800

    fixed api endpoint link

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://protected-wildwood-57753.herokuapp.com/',
+  API_ENDPOINT: 'https://protected-wildwood-57753.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
