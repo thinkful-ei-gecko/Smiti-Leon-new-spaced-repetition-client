ef721b9b482af6f56e3ffec2395a1e60f4bf5e9e
Leon Dail <leondailofficial@gmail.com>
2019-12-16 18:47:03 -0800

    reset config file to point to heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: 'https://protected-wildwood-57753.herokuapp.com/api',
+  API_ENDPOINT: 'https://protected-wildwood-57753.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
-  API_ENDPOINT: 'http://localhost:8000/api'
+  // API_ENDPOINT: 'http://localhost:8000/api'
 }
